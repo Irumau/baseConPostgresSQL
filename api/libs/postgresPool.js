6dbafd134cb0ca13316745cf9454c4536472cb03
Irumau <mauriciovera272@gmail.com>
2023-08-30 03:12:45 -0300

    fix error in connection

--- api/libs/postgresPool.js
+++ api/libs/postgresPool.js
@@ -1,24 +1,24 @@
 const { Pool } = require('pg');
 const { config } = require('../config/config');
 
-let URI = ''
+
+const options = {}
 
 if(config.isProd){
-  URI = config.dbUrl;
+  options.connectionString = config.dbUrl;
+  options.ssl = {
+    rejectUnauthorized: false
+  }
 }else{
   const USER = encodeURIComponent(config.dbUser);
   const PASSWORD = encodeURIComponent(config.dbPassword);
-  URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+  const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+  options.connectionString = URI;
 }
-
 // para conectarnos a nuestro base de datos, si esta la tenemos de manera remota estas mayormente nos dan un URL de conexión. El cual el formato es el siguiente.
-
   //(protocolo)://=>Usuario : => PASSWORD @ => HOST: => PUERTO DONDE CORRE LA DB/ => NOMBRE DE LA BASE DE DATOS
-
-
-
 //Para poder mandar esta URL pool tiene un atributo que este reconoce llamado connectionString: y luego concatenamos la URL.
-const pool = new Pool({ connectionString: URI});
+const pool = new Pool(options);
 
 module.exports = pool;
 
